95424757903d74c89ca52bcd6070f3d799ddeb6a
mbwatson <mattwat@gmail.com>
2016-07-07 15:41:54 -0400

    Add files via upload

---
+++ mapmaker.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(ggmap)
+
+source("./src/map_settings.R")
+source("./src/helpers.R")
+source("./src/init.R")
+
+###########################################################
+###########################################################
+
+## what should each map be named?
+
+## Set manually with
+
+map_title = "Generic Map Title"
+
+## or uncomment the following line to get input from user.
+
+# map_title <- readline('Enter a title for your map(s): ')
+
